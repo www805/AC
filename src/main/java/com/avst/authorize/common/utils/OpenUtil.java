552a2a6cd816cdf70c16c83a2d9ef4e4ab8652cd
zjl <zjl@qq.com>
2019-09-26 19:15:56 +0800

    修改名称

--- src/main/java/com/avst/authorize/common/utils/OpenUtil.java
+++ src/main/java/com/avst/authorize/common/utils/OpenUtil.java
@@ -1067,7 +1067,7 @@ public static String numtoStr(int digit,Integer num){
 		try {
 			String filePath = System.getProperty("user.dir");
 
-			if(filePath.endsWith("authorize")||filePath.endsWith("Accredit")){
+			if(filePath.endsWith("authorize")||filePath.endsWith("Authorize")){
 				if(filePath.indexOf("/") > -1){//Linux截取
 					filePath=filePath.substring(0,filePath.lastIndexOf("/"));//win的截取方式
 				}else{
